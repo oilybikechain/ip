f9bcef510a121973f7f21f64eb6688d43dd69ee5
oilybikechain <oilybikechaindeveloper@gmail.com>
2026-08-21 15:57:06 +0800

    Level-0

---
+++ src/main/java/Sandy.java
@@ -0,0 +1,11 @@
+public class Sandy {
+    public static void main(String[] args) {
+        String banner = " ____                  _       \n"
+                + "/ ___|  __ _ _ __   __| |_   _ \n"
+                + "\\___ \\ / _` | '_ \\ / _` | | | |\n"
+                + " ___) | (_| | | | | (_| | |_| |\n"
+                + "|____/ \\__,_|_| |_|\\__,_|\\__, |\n"
+                + "                         |___/ \n";
+        System.out.println(banner);
+    }
+}
